d271b80f597c0174fcafcc5c107fdb1f8e0e156b
Gabriel Pires <gabpiresdev@gmail.com>
2023-06-26 18:58:47 -0300

    feat: installing and set react redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { Provider as ReduxProvider } from 'react-redux'
 import { AddTodo } from './components/AddTodo'
 import { TodoList } from './components/TodoList'
+import { store } from './store'
 
 export function App() {
   return (
-    <div>
+    <ReduxProvider store={store}>
       <TodoList />
       <AddTodo />
-    </div>
+    </ReduxProvider>
   )
 }
