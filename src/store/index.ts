88ce3ce38e027611ab6fb0f005e2535a3787e056
Gabriel Pires <gabpiresdev@gmail.com>
2023-06-26 18:47:19 -0300

    feat: create todo slice for global store

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+const todoSlice = createSlice({
+  name: 'todo',
+  initialState: ['Fazer café', 'Estudar Redux'],
+  reducers: {},
+})
+
+export const store = configureStore({
+  reducer: {
+    todo: todoSlice.reducer,
+  },
+})
