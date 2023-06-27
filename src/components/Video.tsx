719fc7304a3a8fa448c36d25b5fb5a200f101e84
Gabriel Pires <gabpiresdev@gmail.com>
2023-06-27 19:07:24 -0300

    feat: create a custom hook to get common data of current lesson

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,21 +1,11 @@
 import ReactPlayer from 'react-player'
 import { useDispatch } from 'react-redux'
-import { useAppSelector } from '../store'
-import { next } from '../store/slices/player'
+import { next, useCurrentLesson } from '../store/slices/player'
 
 export function Video() {
   const dispatch = useDispatch()
 
-  const lesson = useAppSelector((state) => {
-    const { currentModuleIndex, currentLessonIndex } = state.player
-
-    const currentLesson =
-      state.player.course.modules[currentModuleIndex].lessons[
-        currentLessonIndex
-      ]
-
-    return currentLesson
-  })
+  const { currentLesson } = useCurrentLesson()
 
   function handlePlayNext() {
     dispatch(next())
@@ -24,7 +14,7 @@ export function Video() {
   return (
     <div className="w-full bg-zinc-500 aspect-video">
       <ReactPlayer
-        url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
         width="100%"
         height="100%"
         controls
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,11 +1,19 @@
 import { MessageCircle } from 'lucide-react'
+import { useEffect } from 'react'
 import { Header } from '../components/Header'
 import { Module } from '../components/Module'
 import { Video } from '../components/Video'
 import { useAppSelector } from '../store'
+import { useCurrentLesson } from '../store/slices/player'
 
 export function Player() {
   const modules = useAppSelector((state) => state.player.course.modules)
+
+  const { currentLesson } = useCurrentLesson()
+
+  useEffect(() => {
+    document.title = `Assistindo ${currentLesson.title}`
+  }, [currentLesson])
 
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center ">
